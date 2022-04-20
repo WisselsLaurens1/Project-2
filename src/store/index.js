7fff3ecb33630ef5b1192e7b1377689c9870ad54
Wouter-Weemaes-IT <78794324+Wouter-Weemaes-IT@users.noreply.github.com>
2022-04-20 14:53:20 +0200

    update ip adress

--- src/store/index.js
+++ src/store/index.js
@@ -19,7 +19,7 @@ export default new Vuex.Store({
       idToken: ""
     },
     endpoints: {
-      login: "http://172.17.50.97:3000/login",
+      login: "http://team17authenticatie-ucllteam08.ucll-ocp-40cb0df2b03969eabb3fac6e80373775-0000.eu-de.containers.appdomain.cloud:8080",
       products: "http://localhost:8000/products"
     },
    },
